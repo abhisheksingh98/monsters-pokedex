42c7e493779a2634708990ddde3b3d2264a87e74
abhisheksingh98 <writetoabhishek07@gmail.com>
2019-10-29 22:50:43 +0530

    Updates

---
+++ precache-manifest.578a071ec6a02ce0fbb4419255408a79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da14ba28efe47a92ca48b7399bfa997",
+    "url": "/monsters-pokedex/index.html"
+  },
+  {
+    "revision": "0bbc1f263a42b412c715",
+    "url": "/monsters-pokedex/static/css/main.39d95d45.chunk.css"
+  },
+  {
+    "revision": "7c7ff7af6890c994a5f1",
+    "url": "/monsters-pokedex/static/js/2.7777846a.chunk.js"
+  },
+  {
+    "revision": "0bbc1f263a42b412c715",
+    "url": "/monsters-pokedex/static/js/main.edfd9f8f.chunk.js"
+  },
+  {
+    "revision": "48f6a18b192e7d0c301c",
+    "url": "/monsters-pokedex/static/js/runtime-main.e1301f06.js"
+  }
+]);
